200dac19d0b14141f60126001c28e7efc53630b6
mrhuytran <dekutran@gmail.com>
2020-03-31 15:03:52 -0700

    change some settings

--- eatapp/settings.py
+++ eatapp/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'eatapp.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ['DATABASE_URL'], conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(default=os.environ['EATVENTURE_DATABASE_URL'], conn_max_age=600, ssl_require=True)
 }
 
 # Password validation
