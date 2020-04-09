2444f83898015d44b14a6edf202ae00acfcb65ad
mrhuytran <dekutran@gmail.com>
2020-04-09 03:02:55 -0700

    update readme

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -5,12 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.render_login),
-    # path('<slug:username>/<slug:password>/', views.login),
     re_path(r'^(?P<username>[^/]+)/(?P<password>[^/]+)/$', views.login),
-    # path('<slug:username>/<slug:password>/deleteaccount', views.delete_account),
     re_path(r'^(?P<username>[^/]+)/(?P<password>[^/]+)/deleteaccount/$', views.delete_account),
     re_path(r'^(?P<username>[^/]+)/(?P<password>[^/]+)/(?P<restaurant_id>[^/]+)/$', views.view_restaurant),
     re_path(r'^(?P<username>[^/]+)/(?P<password>[^/]+)/(?P<restaurant_id>[^/]+)/updaterestaurant/(?P<restaurant_name>[^/]+)/(?P<best_selling_item>[^/]+)/(?P<best_selling_item_dsc>[^/]+)/(?P<food_bank>[^/]+)/$', views.update_restaurant),
     re_path(r'^(?P<username>[^/]+)/(?P<password>[^/]+)/(?P<restaurant_id>[^/]+)/deleterestaurant/$', views.delete_restaurant),
-    # path('<slug:username>/<slug:password>/<slug:restaurant_id>/deleterestaurant/', views.delete_restaurant),
 ]
