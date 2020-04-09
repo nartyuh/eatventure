65f1d08b501c55b9dc3daab28ffb015c73283d40
mrhuytran <dekutran@gmail.com>
2020-04-09 02:38:32 -0700

    debug false. new allowed host

--- eatapp/settings.py
+++ eatapp/settings.py
@@ -24,11 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['EATVENTURE_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'eatventure.herokuapp.com',
+    'eatventure.tranquanghuy.me',
 ]
 
 
