b7e6f66dfec5bf0e1a0f513be220fde2d14e0e78
mrhuytran <dekutran@gmail.com>
2020-04-11 15:31:36 -0700

    wiped cache

--- eatapp/settings.py
+++ eatapp/settings.py
@@ -139,3 +139,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
